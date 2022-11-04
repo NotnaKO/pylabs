9a3e18137c34fc90f8ed79bd2f93c98454f04a83
anton <anton.kopanov@ya.ru>
2022-11-04 11:35:05 +0300

    create drafts main.py, plotter.py, visitor.py

---
+++ plotter.py
@@ -0,0 +1,28 @@
+from visitor import Worker
+from pylab import *
+
+
+class Plotter(Worker):
+    def __init__(self):
+        self._x_label = []
+        self._y_label = []
+        self._x_data = []
+        self._y_data = []
+
+    def accept(self, visitor) -> None:
+        self._x_data, self._y_data = visitor.data
+        self._x_label, self._y_label = visitor.labels
+        for i in range(len(self._x_data)):
+            current_type = visitor.types[i]
+            xlabel(self._x_label)
+            ylabel(self._y_label)
+            match current_type:
+                case "scatter":
+                    scatter(self._x_data[i], self._y_data[i],
+                            legend=visitor.legends[i])
+
+                    # TODO: дописать различных типов графиков
+                case _:
+                    plot(self._x_data[i], self._y_data[i],
+                         legend=visitor.legends[i])
+        savefig(visitor.file_name)
